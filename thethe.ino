9c64061ed053957e654ca8fe3c16f48c00083740
raltamirano <rodrigo.altamirano@gmail.com>
2022-03-24 17:12:02 -0300

    Re-engineering

--- thethe.ino
+++ thethe.ino
@@ -1,189 +1,223 @@
-#include <NewPing.h>
 #include <MIDI.h>
+#include <NewPing.h>
 
-// General constants
-const int ANTENNA1 = 1;                         // Antenna 1
-const int ANTENNA2 = 2;                         // Antenna 2
-const int SENSE_DELAY_MS = 15;                  // Pause in millis before reading antennas again
-const int NO_NOTE = -1;                         // No note detected
-const int NO_CONTROLLER_VALUE = -1;             // No controller value detected
-const double FREE_FIRST_CM = 4.0;               // Free space before start sensing antennas 
-const double MAX_DISTANCE_CM = 60.0;            // Max recognized/allowed distance for both antennas
-const int DEFAULT_VELOCITY = 100;               // Default velocity
-const int SONAR_ITERATIONS = 5;                 // Number of sonar iterations to loop for better measurements
-const int MAX_CONTROLLER_VALUE = 127;           // Max value for the controller (antenna 2) value
-const int INTERVALS_PER_SCALE = 12;             // Max number of intervals from root of every scale
+#define MAX_SENSORS        2
+#define TRIGGER1_PIN       2
+#define ECHO1_PIN          3
+#define TRIGGER2_PIN       4
+#define ECHO2_PIN          5
+#define MAX_DISTANCE_CM   55
+#define DEAD_ZONE_CM       5
 
-// Scales
-const int CHROMATIC = 0;
-const int MAJOR = 1;
-const int MINOR = 2;
-const int HARMONIC_MINOR = 3;
-const int MELODIC_MINOR = 4;
-const int PENTATONIC_MAJOR = 5;
-const int PENTATONIC_MINOR = 6;
-const int BLUES = 7;
+#define NOTE               0
+#define CC                 1
+#define BEND               2
+#define DISABLED          99
 
-const int SCALE_INTERVALS_TO_OCTAVE[] = {  
-  12,
-  7,
-  7,
-  7,
-  7,
-  5,
-  5,
-  6
-};
+#define DEFAULT_PINGS      3
+#define CHROMATIC          0
+#define MAJOR              1
+#define MINOR              2
+#define HARMONIC_MINOR     3
+#define MAJOR_PENTATONIC   4
+#define MINOR_PENTATONIC   5
+#define WHOLE_TONE         6
+#define GM_BASSDRUM_SNARE 70
+#define ONE_FOUR_FIVE     71
 
-const int SCALE_INTERVALS[][12] = {  
-  {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-  {2, 2, 1, 2, 2, 2, 1, 0, 0, 0, 0, 0},
-  {2, 1, 2, 2, 1, 2, 2, 0, 0, 0, 0, 0},
-  {2, 1, 2, 2, 1, 3, 1, 0, 0, 0, 0, 0},
-  {2, 1, 2, 2, 2, 2, 1, 0, 0, 0, 0, 0},
-  {2, 2, 3, 2, 3, 0, 0, 0, 0, 0, 0, 0},
-  {3, 2, 2, 3, 2, 0, 0, 0, 0, 0, 0, 0},
-  {3, 2, 1, 1, 3, 2, 0, 0, 0, 0, 0, 0}
-};
+#define CC_CENTER         64
+#define CC_NO_RESET       -1
 
-// Antenna 1 (note) pin numbers
-const int ANTENNA1_TRIGGER_PIN = 2;
-const int ANTENNA1_ECHO_PIN = 3;
-
-// Antenna 2 (volume/other) pin numbers
-const int ANTENNA2_TRIGGER_PIN = 7;
-const int ANTENNA2_ECHO_PIN = 8;
-
-// Sonars
-NewPing SONAR1 (ANTENNA1_TRIGGER_PIN, ANTENNA1_ECHO_PIN, MAX_DISTANCE_CM + FREE_FIRST_CM);
-NewPing SONAR2 (ANTENNA2_TRIGGER_PIN, ANTENNA2_ECHO_PIN, MAX_DISTANCE_CM + FREE_FIRST_CM);
-
-// Controllers
-const int VELOCITY = -1;
-const int MODULATION = 1;
-const int PORTAMENTO_TIME = 5;
-const int VOLUME = 7;
-const int BALANCE = 8;
-const int PAN = 10;
-const int EXPRESSION = 11;
+#define BASS_DRUM         35
+#define SNARE             38
 
 
-// ---------------------------------------------
-// ---------------- State ----------------------
-// ---------------------------------------------
-// Scale
-int scale = PENTATONIC_MINOR;
-// Octaves
-int octaves = 1;
-// This is what antenna 2 is controlling
-int controllerNumber = MODULATION;
-// Last played note
-int lastNote = NO_NOTE;
-// MIDI output chanel
-int midiChannel = 1;
-// Base MIDI note
-int baseNote = 60; // Middle C
-// Last controller value
-int lastControllerValue = NO_CONTROLLER_VALUE;
-// Reverse distance meaning for antenna 1?
-boolean reverseDistanceMeaningAntenna1 = false;
-// Reverse distance meaning for antenna 2?
-boolean reverseDistanceMeaningAntenna2 = false;
-// Flip antennas (antenna 1 becomes antenna 2 and viceversa)
-boolean flipAntennas = false;
-// ---------------------------------------------
+// Configuration
+int CHROMATIC_INTERVALS[] = { 1 };
+int MAJOR_INTERVALS[] = {2, 2, 1, 2, 2, 2, 1};
+int MINOR_INTERVALS[] = {2, 1, 2, 2, 1, 2, 2};
+int HARMONIC_MINOR_INTERVALS[] = { 2, 1, 2, 2, 1, 3, 1 };
+int MAJOR_PENTATONIC_INTERVALS[] = { 2, 2, 3, 2, 3 };
+int MINOR_PENTATONIC_INTERVALS[] = { 3, 2, 2, 3, 2 };
+int WHOLE_TONE_INTERVALS[] = { 2, 2, 2, 2, 2, 2 };
 
-// MIDI init
+// General parameters
+int sensors = 2;
+int pings[MAX_SENSORS] = {DEFAULT_PINGS, DEFAULT_PINGS};
+boolean reversed[MAX_SENSORS] = {false, true};
+int steps[MAX_SENSORS] = {6, 3};                             // How many divisions on the measureable space (MAX_DISTANCE_CM)
+int channel[MAX_SENSORS] = {1, 2};
+int mode[MAX_SENSORS] = {NOTE, NOTE};
+
+// CC mode-related parameters
+int cc[MAX_SENSORS] = {1, 22};
+int ccNoReadingValue[MAX_SENSORS] = {CC_CENTER, CC_NO_RESET};
+
+// NOTE mode-related parameters
+int root[MAX_SENSORS] = {60, 36};
+int scale[MAX_SENSORS] = {MINOR_PENTATONIC, ONE_FOUR_FIVE};
+int notes[MAX_SENSORS][128];
+boolean latch[MAX_SENSORS] = {true, true};
+boolean noReadingEndsLatch[MAX_SENSORS] = {true, true};       // 'noReadingEndsLatch = false': 100% true latching
+int velocity[MAX_SENSORS] = {100, 100};
+int fixedDuration[MAX_SENSORS] = {250, 10};                   // For non-latching option
+
+// Tracking & State
+int last[MAX_SENSORS] = { -1, -1};
+
+
+// Program
 MIDI_CREATE_DEFAULT_INSTANCE();
 
-void setup()
-{
-  // Setup pins for both antennas
-  pinMode(ANTENNA1_TRIGGER_PIN, OUTPUT);
-  pinMode(ANTENNA1_ECHO_PIN, INPUT);
-  pinMode(ANTENNA2_TRIGGER_PIN, OUTPUT);
-  pinMode(ANTENNA2_ECHO_PIN, INPUT);
-  
-  // Prepare for MIDI I/O
-  MIDI.begin(); 
+void setup() {
+  for (int s = 0; s < MAX_SENSORS; s++)
+    defineScaleNotes(s);
+
+  MIDI.begin();
   Serial.begin(115200);
 }
 
+NewPing S[] = {NewPing (TRIGGER1_PIN, ECHO1_PIN, MAX_DISTANCE_CM + DEAD_ZONE_CM), NewPing (TRIGGER2_PIN, ECHO2_PIN, MAX_DISTANCE_CM + DEAD_ZONE_CM)};
 
-void loop()
-{
-  int note = readNote();
-  int controllerNumber = readControllerNumber();
-  int controllerValue = readControllerValue();
+void loop() {
+  int value;
 
-  int velocity = controllerNumber == VELOCITY ? controllerValue : DEFAULT_VELOCITY;
+  for (int s = 0; s < sensors; s++) {
+    value = -1;
 
-  // Clear current note, if any, if needed
-  if (lastNote != NO_NOTE && note != lastNote) 
-    noteOn(lastNote, 0, midiChannel);
-    
-  // Play new note, if any
-  if (note != NO_NOTE && note != lastNote)
-    noteOn(note, velocity, midiChannel);  
-  lastNote = note;
+    switch (mode[s]) {
+      case NOTE:
+        value = readNote(s);
+        if (value > 0) {
+          if (last[s] != value) {
+            if (latch[s] && last[s] != -1)
+              MIDI.sendNoteOn(last[s], 0, channel[s]);
 
-  if (controllerNumber != VELOCITY && controllerValue != NO_CONTROLLER_VALUE && controllerValue != lastControllerValue) {
-    controlChange(controllerNumber, controllerValue, midiChannel);
-    lastControllerValue = controllerValue;
+            last[s] = value;
+            //velocity[s] = ???; // read from another sensor!
+            MIDI.sendNoteOn(last[s], velocity[s], channel[s]);
+
+            if (!latch[s]) {
+              delay(fixedDuration[s]);
+              MIDI.sendNoteOn(last[s], 0, channel[s]);
+            }
+          }
+        } else {
+          if (latch[s] && noReadingEndsLatch[s] && last[s] != -1) {
+            MIDI.sendNoteOn(last[s], 0, channel[s]);
+            last[s] = -1;
+          }
+        }
+        break;
+
+      case CC:
+        value = readCC(s);
+        if (value > 0) {
+          if (last[s] != value) {
+            MIDI.sendControlChange(cc[s], value, channel[s]);
+            last[s] = value;
+          }
+        } else {
+          if (ccNoReadingValue[s] != CC_NO_RESET) {
+            MIDI.sendControlChange(cc[s], ccNoReadingValue[s], channel[s]);
+            last[s] = ccNoReadingValue[s];
+          }
+        }
+        break;
+
+      case BEND:
+        value = readPitchBend(s);
+        if (last[s] != value) {
+          MIDI.sendPitchBend(value, channel[s]);
+          last[s] = value;
+        }
+        break;
+    }
+  } // for(sensors)
+}
+
+int readNote(int sensor) {
+  int distance = readDistanceCM(sensor);
+  if (distance <= DEAD_ZONE_CM || distance >= MAX_DISTANCE_CM + DEAD_ZONE_CM) return 0;
+
+  int netDistance = distance - DEAD_ZONE_CM;
+  return notes[sensor][
+           reversed[sensor] ?
+           map(netDistance, 0, MAX_DISTANCE_CM, steps[sensor], 0) - 1 :
+           map(netDistance, 0, MAX_DISTANCE_CM, 0, steps[sensor])
+         ];
+}
+
+int readCC(int sensor) {
+  int distance = readDistanceCM(sensor);
+  if (distance <= DEAD_ZONE_CM) return 0;
+
+  int netDistance = distance - DEAD_ZONE_CM;
+  return reversed[sensor] ?
+         map(netDistance, 0, MAX_DISTANCE_CM, 127, 0) :
+         map(netDistance, 0, MAX_DISTANCE_CM, 0, 127);
+}
+
+int readPitchBend(int sensor) {
+  int distance = readDistanceCM(sensor);
+  if (distance <= DEAD_ZONE_CM) return 0;
+
+  int netDistance = distance - DEAD_ZONE_CM;
+  return reversed[sensor] ?
+         map(netDistance, 0, MAX_DISTANCE_CM, 8191, -8192) :
+         map(netDistance, 0, MAX_DISTANCE_CM, -8192, 8191);
+}
+
+int readDistanceCM(int sensor) {
+  return pings[sensor] > 1 ? S[sensor].convert_cm(S[sensor].ping_median(pings[sensor])) : S[sensor].ping_cm();
+}
+
+void defineScaleNotes(int sensor) {
+  notes[sensor][0] = root[sensor];
+  int numberOfIntervals = 0;
+  int intervals[12];
+  switch (scale[sensor]) {
+    case CHROMATIC:
+      numberOfIntervals = 1;
+      memcpy(intervals, CHROMATIC_INTERVALS, sizeof(CHROMATIC_INTERVALS));
+      break;
+    case MAJOR:
+      numberOfIntervals = 7;
+      memcpy(intervals, MAJOR_INTERVALS, sizeof(MAJOR_INTERVALS));
+      break;
+    case MINOR:
+      numberOfIntervals = 7;
+      memcpy(intervals, MINOR_INTERVALS, sizeof(MINOR_INTERVALS));
+      break;
+    case HARMONIC_MINOR:
+      numberOfIntervals = 7;
+      memcpy(intervals, HARMONIC_MINOR_INTERVALS, sizeof(HARMONIC_MINOR_INTERVALS));
+      break;
+    case MAJOR_PENTATONIC:
+      numberOfIntervals = 5;
+      memcpy(intervals, MAJOR_PENTATONIC_INTERVALS, sizeof(MAJOR_PENTATONIC_INTERVALS));
+      break;
+    case MINOR_PENTATONIC:
+      numberOfIntervals = 5;
+      memcpy(intervals, MINOR_PENTATONIC_INTERVALS, sizeof(MINOR_PENTATONIC_INTERVALS));
+      break;
+    case WHOLE_TONE:
+      numberOfIntervals = 6;
+      memcpy(intervals, WHOLE_TONE_INTERVALS, sizeof(WHOLE_TONE_INTERVALS));
+      break;
+    case GM_BASSDRUM_SNARE:
+      steps[sensor] = 2;
+      notes[sensor][0] = BASS_DRUM;
+      notes[sensor][1] = SNARE;
+      return;
+    case ONE_FOUR_FIVE:
+      steps[sensor] = 3;
+      notes[sensor][0] = root[sensor];     // root
+      notes[sensor][1] = root[sensor] + 5; // perfect fourth
+      notes[sensor][2] = root[sensor] + 7; // perfect fifth;
+      return;
   }
-  
-  delay(SENSE_DELAY_MS);
-}
 
-void noteOn(int note, int velocity, int channel) {
-  MIDI.sendNoteOn(note, velocity, channel);
-}
-
-void controlChange(int controller, int value, int channel) {
- MIDI.sendControlChange(controller, value, channel);
-}
-
-int readNote() {
-  double distanceInCm = distance(ANTENNA1) - FREE_FIRST_CM;
-  if (distanceInCm <= 0 || distanceInCm >= MAX_DISTANCE_CM) return NO_NOTE;
-
-  const int intervalsToOctave = SCALE_INTERVALS_TO_OCTAVE[scale];
-  const int totalIntervals = intervalsToOctave * octaves;
-  const double intervalSpaceInCm = MAX_DISTANCE_CM / (totalIntervals + octaves);
-  const int intervals = floor(distanceInCm / intervalSpaceInCm);
-
-  int note = baseNote;
-  for(int i = 0; i < intervals; i++)
-    note += SCALE_INTERVALS[scale][i % intervalsToOctave];
-    
-  return note;
-}
-
-int readControllerNumber() {
-  return controllerNumber;
-}
-
-int readControllerValue() {
-  double realDistance = distance(ANTENNA2);
-  if (realDistance == 0.0) return NO_CONTROLLER_VALUE;
-  
-  double distanceInCm = realDistance - FREE_FIRST_CM;
-  if (distanceInCm <= 0 || distanceInCm >= MAX_DISTANCE_CM) return NO_CONTROLLER_VALUE;
-  
-  int value = distanceInCm / MAX_DISTANCE_CM * MAX_CONTROLLER_VALUE;
-  return reverseDistanceMeaningAntenna2 ? (MAX_CONTROLLER_VALUE - value) : value;
-}
-
-double distance(int antenna) {
-  int reading = 0;
-  if (flipAntennas)
-    reading = (antenna == 2) ? 
-      (SONAR1.ping_median(SONAR_ITERATIONS)) : 
-      (SONAR2.ping_median(SONAR_ITERATIONS));
-  else
-    reading = (antenna == 1) ? 
-        (SONAR1.ping_median(SONAR_ITERATIONS)) : 
-        (SONAR2.ping_median(SONAR_ITERATIONS));
-  
-  return reading / 29.0 / 2.0;
+  for (int i = 1; i < steps[sensor]; i++)
+    notes[sensor][i] = notes[sensor][i - 1] + intervals[(i - 1) % numberOfIntervals];
 }
